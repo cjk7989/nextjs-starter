6c4e91b668216c5e30b8c2d4da4fdac871b26bc9
Jikun <36817715+cjk7989@users.noreply.github.com>
2026-01-06 14:05:58 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Deploy your Next.js site to a public URL with Azure Static Web Apps. v2
+            Deploy your Next.js site to a public URL with Azure Static Web Apps. v3
           </p>
         </a>
       </div>
